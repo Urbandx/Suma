6d4d1775c296281ca19cebeed0ef4104031fd3af
Urbandx <wesker_outbreak@hotmail.com>
2024-09-10 09:25:44 -0600

    Add files via upload

---
+++ sumar_valores.php
@@ -0,0 +1,8 @@
+<?php
+    $valor1 = $_POST['v1'];
+    $valor2 = $_POST['v2'];
+    $valor3 = $_POST['v3'];
+    $valor4 = $_POST['v4'];
+    $suma=$valor1+$valor2+$valor3+$valor4;
+    echo($suma);
+?>
